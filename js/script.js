d1b96052039029ad0e5f03a711fb61faced3ad10
habudala <habudala@live.com>
2016-03-17 19:50:25 -0400

    Rebuilding Calculator in Native JS

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function run1 () {
+	document.calc.input.value += "1";}
+function run2 () {
+	document.calc.input.value += "2";}
+function run3 () {
+	document.calc.input.value += "3";}
+function run4 () {
+	document.calc.input.value += "4";}
+function run5 () {
+	document.calc.input.value += "5";}
+function run6 () {
+	document.calc.input.value += "6";}
+function run7 () {
+	document.calc.input.value += "7";}
+function run8 () {
+	document.calc.input.value += "8";}
+function run9 () {
+	document.calc.input.value += "9";}
+function run0 () {
+	document.calc.input.value += "0";}
+function runMult () {
+	document.calc.input.value += "*";}
+function runSubtr () {
+	document.calc.input.value += "-";}
+function runAdd () {
+	document.calc.input.value += "+";}
+function runDiv () {
+	document.calc.input.value += "/";}
+
+
+function runEqu () {
+	var evalo = eval(document.calc.input.value);
+	document.calc.input.value = evalo;
+	}
+
+function runDel () {
+	document.calc.input.value = " "
+	}
+
+
+
+
+
+
+
+
+
